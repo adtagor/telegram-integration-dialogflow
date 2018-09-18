df409d1d227e89e4b3ef3a9cd3e6ce582dd71c23
adtagor <42305160+adtagor@users.noreply.github.com>
2018-09-18 11:39:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ OPTIONS = argparse.ArgumentParser(prog='run.py',
                                   description='A Telegram Bot who answers to all of your questions')
 OPTIONS.add_argument('--TELEGRAM_TOKEN', help='673895642:AAGvtuwlxnUNuysUu7mPifNLAHIWJ0L0uvk', required=True)
 OPTIONS.add_argument('--ADMIN_CHAT_ID', help='Insert Telegram ChatID', nargs="*", default=[])
-OPTIONS.add_argument('--DIALOGFLOW_KEY', help='Specify Dialogflow Key Path', required=True)
+OPTIONS.add_argument('--DIALOGFLOW_KEY', help='0193f2bda4ff4c9f9831ff89ba4b79c2', required=True)
 OPTIONS.add_argument('--WIT_TOKEN', help='Specify Wit Token', default='')
 OPTIONS.add_argument('--LANG', help='Specify language cod', default='en')
 OPTIONS.add_argument('--log', help='Set logging value', default='DEBUG')
